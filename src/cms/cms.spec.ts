4b5e429678f1ea1ba7bb9f4ce87e0e5fa5f9461c
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-11-20 13:51:42 +0000

    book names must be unique

--- src/cms/cms.spec.ts
+++ src/cms/cms.spec.ts
@@ -2,17 +2,33 @@
  * @jest-environment node
  */
 
+import { PrismaClient } from "@prisma/client";
 import { generateBook, displayBookAtLayer, defaultBook } from "./cms";
 
 describe("cms", () => {
-  test("generating a book return the default book", async () => {
-    const name = "My test book";
-    const bookId = await generateBook(name);
-    const book = await displayBookAtLayer(bookId);
-    expect(book).toEqual({
-      id: bookId,
-      name,
-      layers: defaultBook.map((value) => expect.objectContaining({ value })),
+  beforeEach(async () => {
+    const prisma = new PrismaClient();
+    await prisma.book.deleteMany();
+  });
+
+  describe("generating books", () => {
+    test("generating a book return the default book", async () => {
+      const name = "My test book";
+      const bookId = await generateBook(name);
+      const book = await displayBookAtLayer(bookId);
+      expect(book).toEqual({
+        id: bookId,
+        name,
+        layers: defaultBook.map((value) => expect.objectContaining({ value })),
+      });
+    });
+
+    test("2 books cannot have the same name", async () => {
+      const name = "My test book";
+      await generateBook(name);
+      await expect(generateBook(name)).rejects.toThrow(
+        "A book with that name already exists"
+      );
     });
   });
 });
--- src/cms/cms.ts
+++ src/cms/cms.ts
@@ -15,22 +15,34 @@ export async function displayBookAtLayer(id: number) {
   });
 
   if (!book) {
+    // TODO test what will happen
     throw new Error("Book not found");
   }
 
   return book;
 }
 export async function generateBook(name: string): Promise<number> {
-  const book = await prisma.book.create({
-    data: {
-      name,
-      layers: {
-        createMany: {
-          data: defaultBook.map((value) => ({ value })),
+  let book;
+  try {
+    book = await prisma.book.create({
+      data: {
+        name,
+        layers: {
+          createMany: {
+            data: defaultBook.map((value) => ({ value })),
+          },
         },
       },
-    },
-  });
+    });
+  } catch (error) {
+    if (
+      error instanceof Error &&
+      error.message.includes("Unique constraint failed on the fields: (`name`)")
+    ) {
+      throw new Error("A book with that name already exists");
+    }
+    throw error;
+  }
   return book.id;
 }
 

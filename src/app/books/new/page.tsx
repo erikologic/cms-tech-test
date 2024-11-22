b375eb4412ed468644a7238202c828a9c9b61522
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-11-22 19:07:44 +0000

    new book

--- src/app/books/new/page.tsx
+++ src/app/books/new/page.tsx
@@ -1,20 +1,20 @@
-import { redirect } from "next/navigation";
-import AddBookForm from "./form";
-import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation';
+import AddBookForm from './form';
+import { cookies } from 'next/headers';
+import { PageTitle } from '@/app/component/page-title';
 
 export default async function NewBook() {
+	const cookieStore = await cookies();
+	const token = cookieStore.get('token')?.value;
 
-  const cookieStore = await cookies()
-  const token = cookieStore.get('token')?.value
+	if (!token) {
+		redirect('/sign-in');
+	}
 
-  if (!token) {
-    redirect("/sign-in");
-  }
-
-  return (
-    <main>
-        <h1>Add book</h1>
-        <AddBookForm token={token} />
-    </main>
-  );
+	return (
+		<main>
+			<PageTitle>Add book</PageTitle>
+			<AddBookForm token={token} />
+		</main>
+	);
 }

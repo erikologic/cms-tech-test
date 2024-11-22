78f5254301752868c3ae001c067bf9489a9c675b
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-11-22 18:45:07 +0000

    books

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -2,6 +2,8 @@ import { cookies } from 'next/headers';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { listBooksFromToken } from '../api/list-books/route';
+import { PageTitle } from '../component/page-title';
+import { LinkButton } from '../component/link-button';
 
 const NoBooks = () => <p>No books yet!</p>;
 
@@ -12,7 +14,10 @@ interface BooksListProps {
 const BooksList = ({ books }: BooksListProps) => (
 	<ul>
 		{books.map(book => (
-			<li key={book.id}>
+			<li
+				key={book.id}
+				className="mt-3 text-lg/6 font-semibold text-gray-900 hover:text-gray-600"
+			>
 				<Link href={`/books/${book.id}`}>{book.name}</Link>
 			</li>
 		))}
@@ -30,10 +35,14 @@ export default async function Home() {
 	const books = await listBooksFromToken(token);
 	const isEmptyList = books.length === 0;
 	return (
-		<main>
+		<>
+			<nav className="mt-4 flex gap-4">
+				<LinkButton href={'/books/new'}>Add book</LinkButton>
+				<LinkButton href={'/api/sign-out'}>Sign out</LinkButton>
+			</nav>
+
+			<PageTitle>Your Books</PageTitle>
 			{isEmptyList ? <NoBooks /> : <BooksList books={books} />}
-			<Link href="/books/new">Add book</Link>
-			<Link href="/api/sign-out">Sign out</Link>
-		</main>
+		</>
 	);
 }

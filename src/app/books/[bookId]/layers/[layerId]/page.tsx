7ff153c98888a6f0ab322f1c73fd6c25e7fe7f23
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-11-22 19:05:38 +0000

    content

--- src/app/books/[bookId]/layers/[layerId]/page.tsx
+++ src/app/books/[bookId]/layers/[layerId]/page.tsx
@@ -1,34 +1,44 @@
-import { redirect } from "next/navigation";
-import { cookies } from 'next/headers'
-import { displayBookWithToken } from "@/app/api/display-book-at-layer/route";
-import Link from "next/link";
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
+import { displayBookWithToken } from '@/app/api/display-book-at-layer/route';
+import { LinkButton } from '@/app/component/link-button';
+import { PageTitle } from '@/app/component/page-title';
 
-export default async function LayersPage({params}: {
-    params: Promise<{ layerId: string, bookId: string }>
+export default async function LayersPage({
+	params,
+}: {
+	params: Promise<{ layerId: string; bookId: string }>;
 }) {
+	const cookieStore = await cookies();
+	const token = cookieStore.get('token')?.value;
 
-    const cookieStore = await cookies()
-    const token = cookieStore.get('token')?.value
+	if (!token) {
+		redirect('/sign-in');
+	}
+	const { layerId: layerSlug, bookId } = await params;
+	const layerId = layerSlug === 'latest' ? undefined : Number(layerSlug);
 
-    if (!token) {
-        redirect("/sign-in");
-    }
-    const { layerId: layerSlug, bookId } = await params;
-    const layerId = layerSlug === "latest" 
-        ? undefined
-        : Number(layerSlug);
+	const values = await displayBookWithToken(token, Number(bookId), layerId);
 
-    const values = await displayBookWithToken(token, Number(bookId), layerId);
+	return (
+		<>
+			<nav className="mt-4 flex gap-4">
+				<LinkButton href={`/books/${bookId}`}>
+					Back to Layers
+				</LinkButton>
+			</nav>
+			<PageTitle>Book content:</PageTitle>
 
-    return (
-        <div>
-            <Link href={`/books/${bookId}`}>Back to Layers</Link>
-            <h1>Values:</h1>
-            <ul>
-                {values.map((value) => (
-                    <li key={value}>{value}</li>
-                ))}                
-            </ul>
-        </div>
-    );
-};
+			<ul>
+				{values.map(value => (
+					<li
+						key={value}
+						className="mt-1 text-lg/6 font-semibold text-gray-900 hover:text-gray-600"
+					>
+						{value}
+					</li>
+				))}
+			</ul>
+		</>
+	);
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} flex flex-col items-center px-8`}
+				className={`${geistSans.variable} ${geistMono.variable} flex flex-col items-center px-8 pb-2`}
 			>
 				{children}
 			</body>

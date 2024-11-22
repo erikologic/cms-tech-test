fa87db693bc2b9f15b13f1ce086d76d261860ebe
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-11-22 18:09:39 +0000

    styled home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,27 @@ export default async function Home() {
 	}
 
 	return (
-		<main>
-			<h1>Abecederies CMS</h1>
-			<Link href="/sign-up">Sign up</Link>
-			<Link href="/sign-in">Sign in</Link>
+		<main className="flex flex-col items-center">
+			<h1 className="text-balance py-6 text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+				Abecederies CMS
+			</h1>
+			<p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+				Kids will lov&apos;it!
+			</p>
+			<div className="flex justify-evenly py-6">
+				<Link
+					href="/sign-up"
+					className="mr-8 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+				>
+					Sign up
+				</Link>
+				<Link
+					className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					href="/sign-in"
+				>
+					Sign in
+				</Link>
+			</div>
 		</main>
 	);
 }
